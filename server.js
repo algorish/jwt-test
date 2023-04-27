963d4badb915b324b3deed2b1acbec1d4485bfb9
Rishabh Singh <rsingh2@cogitate.us>
2023-04-27 18:56:17 +0530

    exp added

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const app = express();
 app.use(express.json());
 
 app.post("/generateJWT", (req, res) => {
+  const {exp} = req.body
   const payload = {
     name: "Rishabh",
   };
-  const token = jwt.sign(payload, "mysecret", { expiresIn: 60 });
-  res.json({ jwt: token });
+  const token = jwt.sign(payload, "mysecret", { expiresIn: exp || 60 });
+  res.json({ jwt: token, exp });
 });
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("Version: 2");
 });
 app.listen(8000, () => {
   console.log("SERVER IS LISTENING AT :8000");
