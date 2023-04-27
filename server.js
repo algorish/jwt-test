bf44828fba85dab812e54c511ab4de218eb34056
Rishabh Singh <rsingh2@cogitate.us>
2023-04-27 18:40:33 +0530

    get route

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ app.post("/generateJWT", (req, res) => {
   const payload = {
     name: "Rishabh",
   };
-  const token = jwt.sign(payload, "mysecret", {expiresIn: 300});
+  const token = jwt.sign(payload, "mysecret", { expiresIn: 60 });
   res.json({ jwt: token });
 });
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 app.listen(8000, () => {
   console.log("SERVER IS LISTENING AT :8000");
 });
